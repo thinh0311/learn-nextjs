c2eb926c56d5d01c9ab0faeb87415c9a1921cd94
thinh0311 <thinhxt2000@gmail.com>
2025-01-21 17:05:26 +0700

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,14 @@
 import "@/app/ui/global.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "Test",
+    default: "Test",
+  },
+  description: "Test",
+  metadataBase: new URL("https://test.com"),
+};
 
 export default function RootLayout({
   children,
--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,6 +1,8 @@
 "use server";
 
+import { signIn } from "@/auth";
 import { sql } from "@vercel/postgres";
+import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
@@ -30,4 +32,26 @@ export async function createInvoice(formData: FormData) {
   `;
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
